2cd58d79e95536542a70eda6448429c953d76850
pratyushHub01 <pratyushpandey1236@gmail.com>
2024-09-10 16:58:56 +0530

    Update AppState.jsx

--- app frontend/src/context/AppState.jsx
+++ app frontend/src/context/AppState.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 const AppState = (props) => {
   // const url = "http://localhost:1000/api";
 
-  const url = https://ecom-backend-07se.onrender.com;
+  const url ="https://ecom-backend-07se.onrender.com";
 
 
   const [products, setProducts] = useState([]);
